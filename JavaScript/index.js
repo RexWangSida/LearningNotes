816f0909969f12bbbcbaa779b8b3651f8669b447
Sida (Rex) Wang <48611450+RexWangSida@users.noreply.github.com>
2019-07-05 03:27:58 -0400

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,7 +2,7 @@
 
 console.log('Hello World')
 
-//declare variable, let and const provides "block scope" in addition to global and local scope
+//declare variable, let and const provides "block scope" in addition to global and function scope
 let name = 'Hello World';
 console.log(name);
 const one = 1;
@@ -32,5 +32,25 @@ function greet(name){
 }
 greet('John');
 greet('Charles');
+
+func = function() {
+  return "Another one";
+}
+console.log(func());
+
+//arrow functions
+arrow1 = () => {
+  return "Arrow function1";
+}
+console.log(arrow1());
+
+arrow2 = () => "Arrow function2";
+console.log(arrow2());
+
+arrow3 = (val) => "Arrow function" + val;
+console.log(arrow3('3'));
+
+arrow4 = val => "Arrow function" + val;
+console.log(arrow4('4'));
 
 //for/while loops are the same as in java
