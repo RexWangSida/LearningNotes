0f500b182916a0504c16c7401410db95aff8f15f
rex <wangs132@mcmaster.ca>
2019-07-04 21:54:42 -0400

    JS updated

---
+++ JavaScript/index.js
@@ -0,0 +1,14 @@
+//javascript code can be seperated from html
+
+console.log('Hello World')
+
+//declare variable
+let name = 'Hello World';
+console.log(name);
+const one = 1;
+console.log(one);
+
+//object
+let person = {
+    
+};
