5f50304b145b038dc7628b14764509c40716a59c
SidaWang <wangs132@mcmaster.ca>
2019-09-16 00:20:08 -0400

    update

--- Full-Stack/Django/first_project/first_project/urls.py
+++ Full-Stack/Django/first_project/first_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from first_app import views
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    path('',views.index,name='index'),
     path('admin/', admin.site.urls),
 ]
